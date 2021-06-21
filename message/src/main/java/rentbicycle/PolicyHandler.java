d989da4c55ed493e214a11a0edef579b0033db06
sej226 <sej96226@gmail.com>
2021-06-22 03:07:23 +0900

    message update

--- message/src/main/java/rentbicycle/PolicyHandler.java
+++ message/src/main/java/rentbicycle/PolicyHandler.java
@@ -19,8 +19,12 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener RentMessage : " + bicycleRented.toJson() + "\n\n");
 
-        // Sample Logic //
-        Message message = new Message();
+        // 메세지는 계속 신규 생성..? findbyId를 하려면..
+        Message message = new Message(); 
+        // message.setMessageId(messageId); //messageId??
+        message.setBicycleId(bicycleRented.getBicycleId());
+        message.setBycycleStatus(bicycleRented.getBicycleStatus());
+        message.setTicketId(bicycleRented.getTicketId());
         messageRepository.save(message);
             
     }
@@ -31,8 +35,10 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener ReturnMessage : " + bicycleReturned.toJson() + "\n\n");
 
-        // Sample Logic //
         Message message = new Message();
+        message.setBicycleId(bicycleReturned.getBicycleId());
+        message.setBycycleStatus(bicycleReturned.getBicycleStatus());
+        message.setTicketId(bicycleReturned.getTicketId());
         messageRepository.save(message);
             
     }
@@ -43,8 +49,10 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener BuyTicketMessage : " + paymentApproved.toJson() + "\n\n");
 
-        // Sample Logic //
         Message message = new Message();
+        message.setPaymentId(paymentApproved.getPaymentId());
+        message.setPaymentStatus(paymentApproved.getPaymentStatus());
+        message.setTicketId(paymentApproved.getTicketId());
         messageRepository.save(message);
             
     }
@@ -55,8 +63,10 @@ public class PolicyHandler{
 
         System.out.println("\n\n##### listener RefundTicketMessage : " + paymentCancelled.toJson() + "\n\n");
 
-        // Sample Logic //
         Message message = new Message();
+        message.setPaymentId(paymentCancelled.getPaymentId());
+        message.setPaymentStatus(paymentCancelled.getPaymentStatus());
+        message.setTicketId(paymentCancelled.getTicketId());
         messageRepository.save(message);
             
     }
