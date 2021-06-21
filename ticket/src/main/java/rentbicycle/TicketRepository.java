c770a7ca6c7b7f7ea6d4b0cafd37f4ca7ab7c698
hee018 <86198066+hee018@users.noreply.github.com>
2021-06-21 15:45:58 +0900

    ticket에 checkTicketStatus 함수 추가

---
+++ ticket/src/main/java/rentbicycle/TicketRepository.java
@@ -0,0 +1,10 @@
+package rentbicycle;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="tickets", path="tickets")
+public interface TicketRepository extends PagingAndSortingRepository<Ticket, >{
+
+
+}
