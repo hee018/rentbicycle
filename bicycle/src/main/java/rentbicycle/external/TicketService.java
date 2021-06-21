8f42a97c74f44df7e38d600036f91ded2b452398
beavis80 <83382676+beavis80@users.noreply.github.com>
2021-06-21 23:46:26 +0900

    Update TicketService.java

--- bicycle/src/main/java/rentbicycle/external/TicketService.java
+++ bicycle/src/main/java/rentbicycle/external/TicketService.java
@@ -5,13 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 
-@FeignClient(name="ticket", url="http://ticket:8080")
+@FeignClient(name="ticket", url="http://ticket:8081")
 public interface TicketService {
 
     @RequestMapping(method= RequestMethod.GET, path="/tickets")
-    public void chkTicketStatus(@RequestBody Ticket ticket);
+    public boolean chkTicketStatus(@RequestParam("ticketId") Long ticketId, 
+                                   @RequestParam("usingTime") Long usingTime);
 
 }
