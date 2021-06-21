3389df4d7f8706e9f03ae76246d301ccba16b596
beavis80 <83382676+beavis80@users.noreply.github.com>
2021-06-21 23:06:43 +0900

    Update Bicycle.java

--- bicycle/src/main/java/rentbicycle/Bicycle.java
+++ bicycle/src/main/java/rentbicycle/Bicycle.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Table(name="Bicycle_table")
 public class Bicycle {
 
-    @Id  
+    @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long bicycleId;
     private String bicycleStatus;
@@ -28,33 +28,41 @@ public class Bicycle {
     @PostUpdate
     public void onPostUpdate(){
 
-        //Following code causes dependency to external APIs
-        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+        if( this.getBicycleStatus().equals("Registerd") || this.getBicycleStatus().equals("Returned") ) {
+            //Following code causes dependency to external APIs
+            // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
 
-        // rentbicycle.external.Ticket ticket = new rentbicycle.external.Ticket();
-        // mappings goes here
-        boolean rslt = true;
-        //boolean rslt = BicycleApplication.applicationContext.getBean(rentbicycle.external.TicketService.class)
-        //    .chkTicketStatus(this.getTicketId());
+            // rentbicycle.external.Ticket ticket = new rentbicycle.external.Ticket();
+            // mappings goes here
+            //boolean rslt = true;
+            boolean rslt = BicycleApplication.applicationContext.getBean(rentbicycle.external.TicketService.class)
+                .chkTicketStatus(this.getTicketId());
 
-        if (rslt) {
-            BicycleRented bicycleRented = new BicycleRented();
-            BeanUtils.copyProperties(this, bicycleRented);
-            bicycleRented.publishAfterCommit();
+            if (rslt) {
+                BicycleRented bicycleRented = new BicycleRented();
+                BeanUtils.copyProperties(this, bicycleRented);
+                bicycleRented.publishAfterCommit();
+            }
         }
 
-
-        BicycleReturned bicycleReturned = new BicycleReturned();
-        BeanUtils.copyProperties(this, bicycleReturned);
-        bicycleReturned.publishAfterCommit();
+        if( this.getBicycleStatus().equals("Rented") ) {
+            BicycleReturned bicycleReturned = new BicycleReturned();
+            BeanUtils.copyProperties(this, bicycleReturned);
+            bicycleReturned.publishAfterCommit(); 
+        }
     }
 
     @PreRemove
     public void onPreRemove(){
 
-        BicycleDeleted bicycleDeleted = new BicycleDeleted();
-        BeanUtils.copyProperties(this, bicycleDeleted);
-        bicycleDeleted.publishAfterCommit();
+        // 렌트중인 자전거는 삭제 불가함
+        if ( !this.getBicycleStatus().equals("Rented") )
+        {
+            // 이벤트 발생 > BicycleDeleted
+            BicycleDeleted bicycleDeleted = new BicycleDeleted();
+            BeanUtils.copyProperties(this, bicycleDeleted);
+            bicycleDeleted.publishAfterCommit();
+        }
 
     }
 
